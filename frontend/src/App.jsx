b737a13d9fa0f98bd8eea5fcc9783cdd23734f3e
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-09-22 14:48:54 +0800

    Adding dummy products for display. Hard coded first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <HomeScreen />
     </>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,60 @@
 import logo from "../assets/image/swift.png";
+import { CgProfile } from "react-icons/cg";
+import { LuShoppingCart } from "react-icons/lu";
 
 const Header = () => {
   return (
     <header>
-      <nav className="bg-primary">
-        <div className="mx-auto max-w-7x1 items-center justify-between px-2 py-3 sm:px-6 lg:px-8">
-          <div className="w-32 flex align-center justify-between">
-            <img className="w-9 h-9" src={logo} alt="logo" />
-            <p className="font-poppins font-semibold text-base pt-2">
-              Swift Cart
-            </p>
+      <nav>
+        <div className="mx-auto max-w-7x1 flex items-center justify-between px-2 py-3 sm:px-6 lg:px-8">
+          {/* Left-side nav links */}
+          <ul className="flex items-center space-x-4">
+            <li>
+              <a href="#" className="font-poppins uppercase font-semibold">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-poppins uppercase font-semibold">
+                Shop
+              </a>
+            </li>
+          </ul>
+
+          {/* Center logo (remains fixed in the center) */}
+          <div className="absolute left-1/2 transform -translate-x-1/2">
+            <img className="w-12 h-12" src={logo} alt="logo" />
+          </div>
+
+          {/* Right-side nav links */}
+          <ul className="flex items-center space-x-4">
+            <li>
+              <a href="#" className="font-poppins uppercase">
+                {" "}
+                <CgProfile className="w-6 h-6 text-gray-700 hover:text-black" />
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-poppins uppercase">
+                <LuShoppingCart className="w-6 h-6 text-gray-700 hover:text-black" />
+              </a>
+            </li>
+          </ul>
+
+          {/* Mobile view */}
+          <div className="sm:hidden" id="mobile-menu">
+          <ul className="flex items-center space-x-4">
+            <li>
+              <a href="#" className="font-poppins uppercase font-semibold">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-poppins uppercase font-semibold">
+                Shop
+              </a>
+            </li>
+          </ul>
           </div>
         </div>
       </nav>
