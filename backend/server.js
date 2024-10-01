e2fa8ac1c1fba8eea64c11a18a2ed8c657d0a8af
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-10-01 17:57:07 +0800

    Error Middleware creation

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 dotenv.config()
 import connectDB from './config/config.js';
 import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT || 5000;
 connectDB()
@@ -15,13 +16,8 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products/', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
