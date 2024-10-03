52f38a8125cf55124afc3477557a9758f437c8b3
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-10-03 21:19:20 +0800

    Fetching products are now in rtk

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,35 +1,27 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import Product from "../components/Product.jsx";
 import { Link } from "react-router-dom";
+import { useGetProductsQuery } from "../slices/productsApiSlice.js";
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("http://localhost:8000/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
   return (
-    <div className="bg-white min-h-full">
+    <div className="bg-white min-h-screen">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <Link
-              key={product._id}
-              to={`/product/${product._id}`}
-              className="group"
-            >
-              <Product product={product} />
-            </Link>
-          ))}
-        </div>
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : isError ? (
+          <div>{error?.data?.message || error.error}</div>
+        ) : (
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <Link key={product._id} to={`/product/${product._id}`} className="group">
+                <Product product={product} />
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
