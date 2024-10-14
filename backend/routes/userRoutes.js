b85180229a69aefbbc007deff2276301c6375ffb
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-10-14 20:07:30 +0800

    Setting up userController > authUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,22 @@
 import express from "express";
-const router = express.Router();
-import { authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    deleteUser,
-    getUsersByID,
-    updateUser 
-} from "../controllers/userControllers.js";
-import { protect, admin } from '../middleware/authMiddleware.js'
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUsersByID,
+  updateUser,
+} from "../controllers/userController.js";
 
-router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.post('/logout', logoutUser)
-router.post('/auth', authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUsersByID).put(protect, admin, updateUser);
+const router = express.Router();
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/logout", logoutUser);
+router.post("/auth", authUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUsersByID).put(updateUser);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors'
-dotenv.config()
-import connectDB from './config/config.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import products from './data/products.js';
-import productRoutes from './routes/productRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+import connectDB from "./config/config.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+// import products from './data/products.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
-connectDB()
+connectDB();
 
 const app = express();
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.use('/api/products/', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
