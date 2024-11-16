af2214324241c710dd315df08dc89a4a10cb51a9
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-11-16 19:38:25 +0800

    styling web app name to orange

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="bg-black p-2 m-3 rounded-lg">
       <div className="text-center">
         <p className="font-poppins text-white uppercase font-normal">
-          Swift Cart v2.0 - Gad Ashell Sususco
+         <span className="text-orange-500">Swift Cart v2.0</span> - Gad Ashell Sususco
         </p>
       </div>
       <div className="text-center text-primary font-poppins">
